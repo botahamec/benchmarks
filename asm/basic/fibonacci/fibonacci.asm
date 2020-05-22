cb60a1ae5435fa1f9bc9dcb856cc6db05532b86d
Botahamec <botahamec@outlook.com>
2020-05-22 12:22:49 -0400

    Wrote fibonacci in Assembly

--- asm/basic/fibonacci/fibonacci.asm
+++ asm/basic/fibonacci/fibonacci.asm
@@ -1,10 +1,51 @@
-NUM_OF_NUMBERS equ 50
+NUM_OF_NUMBERS equ 40
 
 section .text
-	global _start
+	global main
+	extern printf
 
-_start:
+	main:
+		mov eax, 0				; a = 0
+		mov ebx, 1				; b = 1
+		mov edx, 0				; c = 0
 
-	mov eax, 1	; System exit with 0
-	mov ebx, 0
-	int 0x80
+		mov ecx, NUM_OF_NUMBERS ; let i = NUM_OF_NUMBERS
+
+	one_num:
+		jcxz end				; while (i > 0)
+
+		mov edx, eax			; c = a
+		add edx, ebx			; c += b
+
+		mov eax, ebx			; a = b
+		mov ebx, edx			; b = c
+
+		mov [vara], eax			; store values before printf call
+		mov [varb], ebx
+		mov [varc], ecx
+		mov [current], ecx
+
+		push edx
+		push line
+		call printf				; printf("%d", c)
+		add esp, 8
+
+		mov eax, [vara]			; get values back
+		mov ebx, [varb]
+		mov edx, [varb]
+		mov ecx, [current]
+
+		dec ecx					; i --
+		jmp one_num				; goto one_num
+
+	end:
+		ret
+
+section .data
+	line db "%d", 10, 0
+
+section .bss
+	vara:		resb	4
+	varb:		resb	4
+	varc:		resb	4
+	current:	resb	4
--- typescript/basic/fibonacci.ts
+++ typescript/basic/fibonacci.ts
@@ -2,7 +2,7 @@
 let a = 0;
 let b = 1;
 let c = 0;
-for (let i = 0; i < 50; i++) {
+for (let i = 0; i < 40; i++) {
 	c = a + b;
 	a = b;
 	b = c;
